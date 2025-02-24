021e59506aeffc1517b92cb3f585ba91e18d97da
Jeremy Tammik <jeremy.tammik@autodesk.com>
2025-02-24 21:01:59 +0100

    initial commit

---
+++ PipeSystemExporter/Command.cs
@@ -0,0 +1,69 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+#endregion
+
+namespace PipeSystemExporter
+{
+  [Transaction(TransactionMode.Manual)]
+  public class Command : IExternalCommand
+  {
+    public Result Execute(
+      ExternalCommandData commandData,
+      ref string message,
+      ElementSet elements)
+    {
+      UIApplication uiapp = commandData.Application;
+      UIDocument uidoc = uiapp.ActiveUIDocument;
+      Document doc = uidoc.Document;
+
+      FilteredElementCollector col
+        = new FilteredElementCollector(doc)
+          .WhereElementIsNotElementType()
+          .OfCategory(BuiltInCategory.OST_PipeCurves)
+          .OfClass(typeof(Pipe));
+
+      int n = col.GetElementCount();
+      Debug.Print($"{n} pipe{Util.PluralSuffix(n)}{Util.DotOrColon(n)}");
+      
+      foreach (Pipe pipe in col)
+      {
+        double diameter = pipe.Diameter;
+        double inch = 25.4;
+        double foot = 12 * inch;
+        int diam_mm = Convert.ToInt32(diameter * foot);
+        List<XYZ> pts = Util.GetConnectorPoints(pipe);
+        Debug.Assert(2 == pts.Count, "expected two endpoints on pipe");
+        Debug.Print($"  pipe '{pipe.Name}' {diam_mm}mm {Util.RealString(diameter)} {Util.PointString(pts[0])} {Util.PointString(pts[1])}");
+      }
+
+      col = new FilteredElementCollector(doc)
+        .WhereElementIsNotElementType()
+        .OfCategory(BuiltInCategory.OST_PipeFitting)
+        .OfClass(typeof(FamilyInstance));
+
+      n = col.GetElementCount();
+      Debug.Print($"{n} fitting{Util.PluralSuffix(n)}{Util.DotOrColon(n)}");
+
+      foreach (FamilyInstance fitting in col)
+      {
+        List<XYZ> pts = Util.GetConnectorPoints(fitting);
+        n = pts.Count;
+        Debug.Assert(1 == n || 2 == n || 3 == n, "expected one, two or three endpoints on fitting");
+        string s = (1 == n) ? "plug" : ((2 == n) ? "elbow" : "tee");
+        string t = string.Join(" ", pts.Select( p => Util.PointString(p)));
+        Debug.Print($"  {s} '{fitting.Symbol.FamilyName}' '{fitting.Name}' {t}");
+      }
+
+      return Result.Succeeded;
+    }
+  }
+}
---
+++ PipeSystemExporter/Util.cs
@@ -0,0 +1,97 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using System.Diagnostics;
+
+namespace PipeSystemExporter
+{
+  class Util
+  {
+    /// <summary>
+    /// Get the connector set of a given element
+    /// </summary>
+    /// <param name="e">the owner of the connector</param>
+    /// <returns>if found, return all the connectors found, or else return null</returns>
+    public static ConnectorSet GetConnectors(Element e)
+    {
+      if (e == null) return null;
+      FamilyInstance fi = e as FamilyInstance;
+      if (fi != null && fi.MEPModel != null)
+      {
+        return fi.MEPModel.ConnectorManager.Connectors;
+      }
+      MEPSystem mepsystem = e as MEPSystem;
+      if (mepsystem != null)
+      {
+        return mepsystem.ConnectorManager.Connectors;
+      }
+      MEPCurve mepcurve = e as MEPCurve;
+      if (mepcurve != null)
+      {
+        return mepcurve.ConnectorManager.Connectors;
+      }
+      return null;
+    }
+
+    /// <summary>
+    /// Get a list of connector points of a given element
+    /// </summary>
+    public static List<XYZ> GetConnectorPoints(Element e)
+    {
+      ConnectorSet cons = GetConnectors(e);
+      int n = cons.Size;
+      List<XYZ> pts = new List<XYZ>(n);
+      foreach (Connector con in cons)
+      {
+        pts.Add(con.Origin);
+      }
+      return pts;
+    }
+
+    /// <summary>
+    ///     Return an English plural suffix for the given
+    ///     number of items, i.e. 's' for zero or more
+    ///     than one, and nothing for exactly one.
+    /// </summary>
+    public static string PluralSuffix(int n)
+    {
+      return 1 == n ? "" : "s";
+    }
+
+    /// <summary>
+    ///     Return a dot (full stop) for zero
+    ///     or a colon for more than zero.
+    /// </summary>
+    public static string DotOrColon(int n)
+    {
+      return 0 < n ? ":" : ".";
+    }
+
+    /// <summary>
+    ///     Return a string for a real number
+    ///     formatted to two decimal places.
+    /// </summary>
+    public static string RealString(double a)
+    {
+      return a.ToString("0.####");
+    }
+
+    /// <summary>
+    ///     Return a string for an XYZ point
+    ///     or vector with its coordinates
+    ///     formatted to two decimal places.
+    /// </summary>
+    public static string PointString(
+        XYZ p,
+        bool onlySpaceSeparator = false)
+    {
+      var format_string = onlySpaceSeparator
+          ? "{0} {1} {2}"
+          : "({0},{1},{2})";
+
+      return string.Format(format_string,
+          RealString(p.X),
+          RealString(p.Y),
+          RealString(p.Z));
+    }
+  }
+}
